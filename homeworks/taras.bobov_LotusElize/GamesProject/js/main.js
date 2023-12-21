50c395ec8f009934df6f1382117b7b9acb626c8d
LotusElize <handmadenumber@gmail.com>
2023-12-21 17:37:13 +0200

    almost deploy
    - Додана адаптивна верстка для оптимального відображення на різних пристроях.
    - Видалені зайві стилі, які не використовуються або є зайвими.
    - Оптимізовані стилі для покращення продуктивності та швидкості завантаження.
    - Перерозподілені файли та створені нові папки для більшої організації коду.

--- homeworks/taras.bobov_LotusElize/GamesProject/js/main.js
+++ homeworks/taras.bobov_LotusElize/GamesProject/js/main.js
@@ -1,15 +1,3 @@
-/**
- * Perform a mathematical operation on
- * two numbers and return the result as a string indicating the number of games.
- * @param {string} firstValue The first number
- * @param {string} secondValue The second number
- * @param {string} operation The operation symbol (+, -, *, /)
- * @returns {string} The number of games as a string, indicating a single game or multiple games
- * - If firstValue or secondValue is not a number, returns 'Enter a number'
- * - If operation is not '+', '-', '*', '/', returns 'Choose a valid operation'
- * - If the result is more than 100, returns 'Result is too big'
- * - Else return the number of games, e.g., 1 '+' 2 returns '1 game'
- */
 function calculate(firstValue, secondValue, operation) {
     const num1 = Number(firstValue);
     const num2 = Number(secondValue);
